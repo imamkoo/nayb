cbe801750c1564c8f95e254c0bfcd7c9d57acc5e
imamkoo <imammm.kooo@gmail.com>
2025-04-20 12:35:24 +0700

    Adding Services component

--- src/components/Steps/Rating.tsx
+++ src/components/Steps/Rating.tsx
@@ -43,7 +43,7 @@ const Rating: React.FC<StepsProps> = ({ name, rating, img, className }) => {
       ref={ref}
       initial={{ opacity: 0, y: 100, scale: 0.95 }}
       whileInView={{ opacity: 1, y: 0, scale: 1 }}
-      viewport={{ once: true }}
+      viewport={{ once: false }}
       transition={{ duration: 0.5, ease: "easeOut" }}
     >
       <div className="bg-grey-300 flex size-17 items-end justify-center overflow-hidden rounded-full">
